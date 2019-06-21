abf4c1854ee0591a640ca9f9cb11e8b2848dee30
Edwin Rueda <ejrueda95g@gmail.com>
2019-06-20 21:28:54 -0300

    v6.0

--- Materias/Visualizaçao da informaçao/Vast Challenge/mini-challenge3/MC3/grafico1/main.py
+++ Materias/Visualizaçao da informaçao/Vast Challenge/mini-challenge3/MC3/grafico1/main.py
@@ -28,7 +28,7 @@ def histogram(df, col, bins=30):
     largura = max(df.index) - min(df.index)
     h = largura/bins
     aux = min(df.index)
-    for i in range(100):
+    for i in range(bins):
         edges.append(aux)
         hist.append(df.loc[aux:aux + h].count()[col])
         aux = aux + h
@@ -45,14 +45,16 @@ p1.yaxis.axis_label = "quantidade de tweets"
 p2 = figure(plot_height=300, plot_width=300, title="Quantidade de tweets por bairro",
            tools="hover,pan,wheel_zoom,box_zoom,reset,save",x_range=(-4,4), y_range=(-4,4))
 #disribuição normalizada
-p3 = figure(plot_height=300, plot_width=600, title="Quantidade de tweets por bairro normaliada",
+p3 = figure(plot_height=300, plot_width=800, title="Quantidade de tweets por bairro normaliada",
            tools="hover,pan,wheel_zoom,box_zoom,reset,save",x_axis_type='datetime', x_range=p1.x_range)
 p3.xaxis.axis_label = "dias registrados"
 p3.yaxis.axis_label = "quantidade de tweets"
-#prueba
-p4 = figure(plot_height=300, plot_width=600, title="Quantidade de tweets por bairro",
-           tools="hover,pan,wheel_zoom,box_zoom,reset,save",x_axis_type='datetime', x_range=p1.x_range)
+#distribuição dos dados por intervalo em cada bairro
+p4 = figure(plot_height=300, plot_width=600, title="Quantidade geral de tweets no intervalo",
+           tools="hover,pan,wheel_zoom,box_zoom,reset,save",x_axis_type='datetime')
  
+p5 = figure(plot_height=300, plot_width=600, title="Quantidade de tweets por bairro no intervalo",
+           tools="hover,pan,wheel_zoom,box_zoom,reset,save",x_axis_type='datetime')
 
 names = ["Palace Hills", "Northwest", "Old Town", "Safe Town", "Southwest", "Downtown",
          "Wilson Forest", "Scenic Vista", "Broadview", "Chapparal", "Terrapin Springs",
@@ -73,7 +75,7 @@ p1.hover.tooltips = [("data inicial", "@left{%F %T}"),
 p1.hover.formatters = {'left': 'datetime', 'right': 'datetime'}
 p1.hover.mode = "vline"
 
-#histograma geral normalizado por bairro
+#histograma geral normalizado
 edges2, hist2 = histogram(data_norm.loc[data_norm.location==bairro_init], "location", bins=100)
 source2 = ColumnDataSource(dict(hist=hist2,left=edges2[:-1], right=edges2[1:]))
 p3.quad(top="hist", bottom=0, left="left", right="right", color = "red",
@@ -98,36 +100,60 @@ p2.hover.tooltips = [("bairro", "@location"),
                      ("quantidade", "@values")]
 
 #histograma geral normalizado no intervalo de 5h até 30h depois do terremoto "2020-04-06 19:40:00":"2020-04-07 20:40:00"
-edges3, hist3 = histogram(data_norm.loc[data_norm.location==bairro_init].loc["2020-04-06 14:40:00":"2020-04-06 19:40:00"],
-                          "location", bins=100)
-source3 = ColumnDataSource(dict(hist=hist2,left=edges2[:-1], right=edges2[1:]))
-p4.quad(top="hist", bottom=0, left="left", right="right", color = "red",
-            line_color="white", alpha=0.7, source=source3, legend="tweets")
+data4 = data_norm.loc["2020-04-06 14:40:00":"2020-04-06 19:40:00"]
+edges4, hist4 = histogram(data4,"location", bins=int(1+3.3*np.log(data4.shape[0])))
+source4 = ColumnDataSource(dict(hist=hist4,left=edges4[:-1], right=edges4[1:]))
+p4.quad(top="hist", bottom=0, left="left", right="right", color = "orange",
+            line_color="white", alpha=0.8, source=source4, legend="tweets")
 p4.hover.tooltips = [("data inicial", "@left{%F %T}"),
                     ("data final", "@right{%F %T}"),
                     ("quantidade", "@hist")]
 p4.hover.formatters = {'left': 'datetime', 'right': 'datetime'}
 p4.hover.mode = "vline"
 
-def update_data(attrname, old, new):
+#histograma por bairro normalizado no intervalo de 5h até 30h depois do terremoto
 
+data5 = data_norm.loc[data_norm.location==bairro_init].loc["2020-04-06 14:40:00":"2020-04-06 19:40:00"]
+edges5, hist5 = histogram(data5,"location", bins=int(1+3.3*np.log(data5.shape[0])))
+source5 = ColumnDataSource(dict(hist=hist5,left=edges5[:-1], right=edges5[1:]))
+p5.quad(top="hist", bottom=0, left="left", right="right", color = "green",
+            line_color="white", alpha=0.8, source=source5, legend="tweets")
+p5.hover.tooltips = [("data inicial", "@left{%F %T}"),
+                    ("data final", "@right{%F %T}"),
+                    ("quantidade", "@hist")]
+p5.hover.formatters = {'left': 'datetime', 'right': 'datetime'}
+p5.hover.mode = "vline"
+
+def update_data(attrname, old, new):
     #Para obtener los valores actuales
-    #day_val = s_day.value
     vec_val = select_vec.value
     #hr_val = s_range.value
+    ht = s_tipo.value
     #Actualizar grafico 3
     edges2, hist2 = histogram(data_norm.loc[data_norm.location==vec_val], "location", bins=100)
     source2.data = dict(hist=hist2,left=edges2[:-1], right=edges2[1:])
-    
+    #Actualizar gráfico4
+    if ht == "5":
+        data4 = data_norm.loc["2020-04-06 14:40:00":"2020-04-06 19:40:00"]
+        data5 = data_norm.loc[data_norm.location==bairro_init].loc["2020-04-06 14:40:00":"2020-04-06 19:40:00"]
+    else:
+        data4 = data_norm.loc["2020-04-06 14:40:00":"2020-04-07 20:40:00"]
+        data5 = data_norm.loc[data_norm.location==bairro_init].loc["2020-04-06 14:40:00":"2020-04-07 20:40:00"]
+    edges4, hist4 = histogram(data4,"location", bins=int(1+3.3*np.log(data4.shape[0])))
+    source4.data = dict(hist=hist4,left=edges4[:-1], right=edges4[1:])
+    #Acutalizar gŕafico5
+    edges5, hist5 = histogram(data5,"location", bins=int(1+3.3*np.log(data5.shape[0])))
+    source5.data = dict(hist=hist5,left=edges5[:-1], right=edges5[1:])
     
 #Para hacer las actualizaciones
 for w in [select_vec, s_tipo]:
     w.on_change('value', update_data)
 
 inputs = column([select_vec, s_tipo], width=200)
+vzio = column(width=210)
 row_1 = row([inputs, p1, p2], width=1100)
-row_2 = row([p3])
-row_3 = row([])
+row_2 = row([vzio, p3])
+row_3 = row([p4, p5])
 row_4 = row([])
 
 curdoc().add_root(row(gridplot([[row_1],[row_2],[row_3],[row_4]]), width=400))
