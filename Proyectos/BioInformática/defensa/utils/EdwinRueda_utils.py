87a007053f6b716fd688d3fe319d2cbd30f68608
Edwin <ejrueda95g@gmail.com>
2020-06-08 21:08:13 -0300

    testing the utils

---
+++ Proyectos/BioInformática/defensa/utils/EdwinRueda_utils.py
@@ -0,0 +1,154 @@
+### for more documentation, you can read the "creating utils" notebook.
+import tensorflow as tf
+import numpy as np
+from time import time
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_score, recall_score
+from sklearn.decomposition import PCA
+tf.keras.backend.set_floatx('float64')
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+class scaler:
+    def __init__(self, xmin, xmax):
+        """
+        minmax scaler from dataframe
+        """
+        self.xmin = xmin
+        self.xmax = xmax
+        self.min_data = False
+        self.max_data = False
+        self.flag = False
+        
+    def fit(self, X):
+        self.min_data = np.min(X).values
+        self.max_data = np.max(X).values
+        self.flag = True
+        
+    def transform(self, X):
+        assert self.flag, "Erro de treinamento, primeiro tem que treinar o Scaler, called .fit()"
+        X_r = X.copy()
+        X_r = ((X_r - self.min_data)/(self.max_data - self.min_data))*(self.xmax-self.xmin) + self.xmin
+        return X_r
+    
+    def inverse_transform(self, X):
+        assert self.flag, "Erro de treinamento, primeiro tem que treinar o Scaler, called .fit()"
+        X_r = X.copy()
+        X_r = ((X_r - self.xmin)*(self.max_data - self.min_data)/(self.xmax - self.xmin)) + self.min_data
+        return X_r
+
+class gan_utils:
+    
+    def __init__(self):
+        self.accumulated_gloss = []
+        self.accumulated_dloss = []
+        self.precision = []
+        self.recall = []
+        self.kl_d = []
+        self.X_train = None
+        self.G = None
+        self.D = None
+        self.noise_input = None
+    
+    @tf.function
+    def kl_divergence(self, probability):
+        probability = tf.clip_by_value(probability, 1e-5, 1-1e-5)
+        return tf.reduce_mean(probability*tf.math.log(probability/(1-probability)))
+    
+    @tf.function
+    def train_step(self, sample, batch_size, loss_function, noise_input, optimizerG, optimizerD):
+        noise = tf.random.normal([batch_size, noise_input])
+        with tf.GradientTape() as gG, tf.GradientTape() as gD:
+            synthetic_data = G(noise, training=True)
+
+            real_output = D(sample, training=True)
+            fake_output = D(synthetic_data, training=True)
+
+            gen_loss = my_binary_cross_entropy(fake_output, tf.ones_like(fake_output))
+            dis_loss = .5*(my_binary_cross_entropy(real_output, tf.ones_like(real_output)) + my_binary_cross_entropy(fake_output,tf.zeros_like(fake_output)))
+
+        g_generator = gG.gradient(gen_loss, G.trainable_variables)
+        g_discriminator = gD.gradient(dis_loss, D.trainable_variables)
+
+        optimizerG.apply_gradients(zip(g_generator, G.trainable_variables))
+        optimizerD.apply_gradients(zip(g_discriminator, D.trainable_variables))
+
+        return gen_loss, dis_loss
+    
+    def train(self, dataset, G, D, noise_input, epochs, batch_size, loss_function, optimizerG, optimizerD):
+        #reset metrics
+        self.accumulated_gloss = []
+        self.accumulated_dloss = []
+        self.precision = []
+        self.recall = []
+        self.kld = []
+        batch_g_loss = []
+        batch_d_loss = []
+        self.G, self.D = G, D
+        self.noise_input = noise_input
+        self.X_train = dataset
+        batches = tf.data.Dataset.from_tensor_slices(dataset).batch(batch_size)
+        for epoch in range(epochs):
+            t_i = time()
+            for batch in batches:
+                batch_g_loss, batch_d_loss = self.train_step(batch, batch_size, loss_function, noise_input,
+                                                             optimizerG, optimizerD)
+            self.accumulated_dloss.append(tf.reduce_mean(batch_d_loss))
+            self.accumulated_gloss.append(tf.reduce_mean(batch_g_loss))
+
+            X_sint = G(tf.random_normal_initializer(mean=0.0, stddev=1)([batch_size,noise_input],
+                                                                        dtype=tf.float64))
+            X_comb = tf.concat([dataset, X_sint], axis=0)
+            y_comb = tf.concat([tf.ones((dataset.shape[0],1)), np.zeros((X_sint.shape[0],1))], axis=0)
+            y_proba = D(X_comb)
+            y_predict = tf.reshape((tf.greater(y_proba, .5).numpy()*1), [-1])
+            self.kl_d.append(self.kl_divergence(y_proba).numpy())
+            self.precision.append(precision_score(y_comb.numpy(), y_predict.numpy()))
+            self.recall.append(recall_score(y_comb.numpy(), y_predict.numpy()))
+            t_f = time()
+            
+            print("epochs[%d:%d] :: G_loss[%f] :: D_loss[%f] :: time:%f[s]"%(epoch, epochs,
+                                                                             self.accumulated_gloss[-1],
+                                                                             self.accumulated_dloss[-1],
+                                                                             t_f-t_i))
+        return self.accumulated_gloss, self.accumulated_dloss
+        
+    def plot_results(self):
+        fig = plt.figure(figsize=(15,10))
+        for i in range(4):
+            plt.subplot(2,2, i+1)
+            if i==0:
+                rr = range(0,len(gloss), 3)
+                short_g_loss = [self.accumulated_gloss[i] for i in rr]
+                short_d_loss = [self.accumulated_dloss[i] for i in rr]
+                plt.plot(rr, short_g_loss, label="Generator", color="#FFB248")
+                plt.plot(rr, short_d_loss, label="Discriminator", color="#5F84B4")
+                plt.xlabel("Epochs", fontsize=13)
+                plt.ylabel("Loss", fontsize=13)
+                plt.grid(axis="y")
+                plt.legend();
+            elif i==1:
+                plt.title("Divergencia de Kullback-Leibler", fontsize=14)
+                plt.plot(range(len(self.kl_d)), self.kl_d, linewidth=.5)
+                plt.plot(range(len(self.kl_d)), np.zeros(len(self.kl_d)))
+                plt.grid()
+                plt.xlabel("Epochs");
+                plt.ylabel("Divergencia de Kullback-Leibler");
+            elif i==2:
+                short_precision = [self.precision[i] for i in rr]
+                plt.plot(range(len(short_precision)), short_precision, linewidth=.8, label="precision")
+                plt.grid(axis="y")
+                plt.plot(range(len(short_precision)), np.ones(len(short_precision))*.5,
+                         label="target", linestyle="--")
+                plt.legend();
+            elif i==3:
+                pca = PCA(n_components=2)
+                X_real_pca = pca.fit_transform(self.X_train.numpy())
+                noise = tf.random.normal([300, self.noise_input])
+                synthetics = self.G(noise)
+                X_fake_pca = pca.transform(synthetics.numpy())
+                plt.scatter(X_fake_pca[:,0], X_fake_pca[:,1], label="synthetic", alpha=.4, color="orange",
+                           edgecolors="red");
+                plt.scatter(X_real_pca[:,0], X_real_pca[:,1], label="real", marker="*", s=80, color="green",
+                            edgecolors="black");
+        return fig
